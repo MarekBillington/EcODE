ddfe2f5a06d9bbf7941352f0fca718217bff11e5
merak <MarekBillington@users.noreply.github.com>
2023-01-22 20:40:11 +0000

    Playing with Rust IO libs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
-use std::process::Command;
-use std::io::{self, Write};
+use std::env;
 
 fn main() {
 
-    let c = cfg!(unix);
+    /*
+     * Two Ways to capture processes
+     * 
+     *  1. Instrument
+     *      Track the direct requests
+     *  2. Sample
+     *      Track via already running processes
+     * 
+     * I think in order to track with voltage and memory usage it needs to be on the instrument
+     * Installation and triggering needs to be easy to bundle
+     */
 
-    let output = Command::new("sh")
-        //.args(["-c"])
-        .args(["perf"])
-        .output()
-        .expect("failed to execute process");
+    let cmds: Vec<String> = env::args().collect();
 
-    println!("{:?}", c);
-    io::stdout().write_all(&output.stdout).unwrap();
+    dbg!(cmds);
+
+    println!("Hello")
+
 }
 
