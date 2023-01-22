d84c66b15c11c1243c9338b7b30a127bf9d64459
merak <MarekBillington@users.noreply.github.com>
2023-01-22 20:54:29 +0000

    Playing with CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+use std::io::{self, Write};
 use std::env;
 
 fn main() {
@@ -16,7 +18,15 @@ fn main() {
 
     let cmds: Vec<String> = env::args().collect();
 
+    let output = String::from("echo ") + &String::from(&cmds[1]);
+    let proc = Command::new("sh")
+        .arg("-c")
+        .arg(output)
+        .output()
+        .expect("failed to execute process");
+
     dbg!(cmds);
+    io::stdout().write_all(&proc.stdout).unwrap();
 
     println!("Hello")
 
