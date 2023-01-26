3da165fc789976535b8382c7a725ec5ecbc0f3b8
merak <MarekBillington@users.noreply.github.com>
2023-01-26 17:11:50 +0000

    Added command output to cli

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use std::process::Command;
 use std::io::{self, Write};
 use std::env;
 
+const PRINT_HELP: &'static str = "
+Want some help?
+Use the following commands:
+    -h  --help  Display help
+    -c  --command   Run command, takes <COMMAND> of either Setup|Run
+";
+
 fn main() {
 
     /*
@@ -14,9 +21,14 @@ fn main() {
      * 
      * I think in order to track with voltage and memory usage it needs to be on the instrument
      * Installation and triggering needs to be easy to bundle
+     * 
+     * Need to get a configuration step going and allow commands..?
      */
 
-    let cmds: Vec<String> = env::args().collect();
+    let mut cmds: Vec<String> = env::args().collect();
+    // Omit the calling command
+    cmds.remove(0);
+
 
     let output = String::from("echo ") + &String::from(&cmds[1]);
     let proc = Command::new("sh")
@@ -25,10 +37,20 @@ fn main() {
         .output()
         .expect("failed to execute process");
 
-    dbg!(cmds);
+    dbg!(&cmds);   
+        
+    // need to remove the first command, then group by actions
+    // i.e. -c <COMMAND>
+    let out = match cmds[0].as_str() {
+        "-h" | "--help" => PRINT_HELP,
+        "-c" => "Run the command (Setup or Run)",
+        _ => "No command given",
+    };
+
+    println!("{}", out);
+
     io::stdout().write_all(&proc.stdout).unwrap();
 
-    println!("Hello")
-
 }
+
 
